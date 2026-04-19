718032317ccc2262f0f1bdd74d36d91faa0a604a
a-saber <ahs629862@gmail.com>
2026-04-19 20:57:03 +0200

    Future builder

--- lib/features/home/views/home_view.dart
+++ lib/features/home/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -5,6 +6,7 @@ import 'package:nti5_firebase/features/auth/data/models/user_model.dart';
 import 'package:nti5_firebase/features/auth/data/repo/auth_repo.dart';
 import 'package:nti5_firebase/features/auth/views/login_view.dart';
 import 'package:nti5_firebase/features/home/cubit/user_data/user_data_cubit.dart';
+import 'package:nti5_firebase/features/home/data/models/task_model.dart';
 
 import '../cubit/user_data/user_data_state.dart';
 import 'new_task_view.dart';
@@ -20,27 +22,79 @@ class HomeView extends StatelessWidget {
           onPressed: (){
         Navigator.push(context, MaterialPageRoute(builder: (context)=> NewTaskView()));
       }),
+      // appBar: AppBar(
+      //   title: BlocProvider(
+      //       create: (context)=> UserDataCubit(AuthRepo())..loadUserData(),
+      //       child: BlocBuilder<UserDataCubit, UserDataState>(
+      //         builder: (context, state) {
+      //           String name = '';
+      //           if(state is UserDataSuccess){
+      //             name = state.userModel.name ?? '';
+      //           }
+      //           return Text('Welcome $name');
+      //
+      //         }
+      //       )),
+      //   actions: [
+      //     IconButton(
+      //       onPressed: ()async{
+      //         await FirebaseAuth.instance.signOut();
+      //         Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context)=> LoginView()), (r)=>false);
+      //       }, icon: Icon(Icons.logout))
+      //   ],
+      // ),
       appBar: AppBar(
-        title: BlocProvider(
-            create: (context)=> UserDataCubit(AuthRepo())..loadUserData(),
-            child: BlocBuilder<UserDataCubit, UserDataState>(
-              builder: (context, state) {
-                String name = '';
-                if(state is UserDataSuccess){
-                  name = state.userModel.name ?? '';
-                }
-                return Text('Welcome $name');
+        title: FutureBuilder(
+          future: FirebaseFirestore.instance.collection('users')
+              .doc(FirebaseAuth.instance.currentUser!.uid).get(),
+          builder: (context, snapshot){
+            if (snapshot.hasError) {
+              return Text("Something went wrong");
+            }
 
-              }
-            )),
-        actions: [
-          IconButton(
-            onPressed: ()async{
-              await FirebaseAuth.instance.signOut();
-              Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context)=> LoginView()), (r)=>false);
-            }, icon: Icon(Icons.logout))
-        ],
+            if (snapshot.hasData && !snapshot.data!.exists) {
+              return Text("Document does not exist");
+            }
+
+            if (snapshot.connectionState == ConnectionState.done) {
+              Map<String, dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
+              UserModel userModel = UserModel.fromJson(data);
+              return Text("Welcome ${userModel.name}");
+            }
+
+            return Text("loading");
+          }
+        ),
       ),
+      body: FutureBuilder(
+        future: FirebaseFirestore.instance.collection('users')
+            .doc(FirebaseAuth.instance.currentUser?.uid).collection('tasks').get(),
+          builder: (context, snapshot){
+          if(snapshot.hasError){
+            return Text("Something went wrong");
+          }
+          else if (snapshot.connectionState == ConnectionState.done) {
+            List<TaskModel> tasks = 
+            snapshot.data!.docs.map((doc)=> TaskModel.fromJson(doc.data())..id = doc.id).toList();
+            return ListView.separated(
+              itemBuilder: (context, index){
+                return Container(
+                  padding: EdgeInsets.all(15),
+                  child: Column(children: [
+                   Text(tasks[index].title??''),
+                   Text(tasks[index].description??''),
+                   Text(tasks[index].dateTime.toString()),
+                  ],),
+                );
+              }, 
+              separatorBuilder: (context, index) => Divider(), 
+              itemCount: tasks.length
+            );
+          }
+          return Text("loading");
+          
+            
+          }),
     );
   }
 }
